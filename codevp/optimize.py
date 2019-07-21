5d4b4e3fbc12714159c8d067803fc5698104f21c
GNiendorf <gavinniendorf@gmail.com>
2019-07-21 11:14:30 -0600

    Optimization bug fixes, requires that c, kappa maintain sign. First useable optimization version.

--- codevp/optimize.py
+++ codevp/optimize.py
@@ -7,46 +7,62 @@ from .raygroup import ray_plane
 
 def update_geometry(inputs, geoparams, vary_dicts):
     """ Return the geometry requested by the optimization algorithm. """
+    vary_idxs = 0
     for dict_ in vary_dicts:
         name = dict_["name"]
         vary_list = dict_["vary"]
         for surface in geoparams:
             try:
                 if surface["name"] == name:
-                    for idx, item in enumerate(vary_list):
-                        surface[item] = inputs[idx]
+                    for item in vary_list:
+                        surface[item] = inputs[vary_idxs]
+                        vary_idxs += 1
             except:
                 continue
     return geoparams
 
-def get_rms(inputs, geoparams, raygroup, vary_dicts):
+def get_rms(inputs, geoparams, vary_dicts):
     """ Return the rms of an updated geometry. """
     params_iter = update_geometry(inputs, geoparams, vary_dicts)
-    raygroup_iter = ray_plane(params_iter, [0., 0., 0.], 1.1, d=[0.,0.,1.], nrays=100)
+    raygroup_iter = ray_plane(params_iter, [0., 0., 0.], 1.1, d=[0.,0.,1.], nrays=50)
     try:
         oplt = optplot(params_iter, raygroup_iter)
         rms = oplt.spotdiagram(optimizer=True)
     except:
         rms = 999.
+    print("RMS of iteration: ", rms)
     return rms
 
-def optimize(geoparams, raygroup, vary_dicts, typeof='least_squares', max_iter=None):
-    """ Optimize a given geometry for a given raygroup and varylist and return the new geometry. """
+def optimize(geoparams, vary_dicts, typeof='least_squares', max_iter=None):
+    """ Optimize a given geometry for a given varylist and return the new geometry. """
     initial_guess = []
+    param_lb = []
+    param_ub = []
     for dict_ in vary_dicts:
         name = dict_["name"]
         vary_list = dict_["vary"]
-        for surface in geoparams:
+        for surf_idx, surface in enumerate(geoparams):
             try:
                 if surface["name"] == name:
                     for idx, item in enumerate(vary_list):
                         attr = surface[item]
                         if item == 'P' and (isinstance(item, float) or isinstance(item, int)):
-                            attr = attr[2]
+                             attr = attr[2]
+                        if item == 'c' or item == 'kappa':
+                            lower_bound = 0 if attr > 0 else -np.inf
+                            upper_bound = np.inf if attr >= 0 else 0.
+                            param_lb.append(lower_bound)
+                            param_ub.append(upper_bound)
+                        else:
+                            param_lb.append(-np.inf)
+                            param_ub.append(np.inf)
+                        
                         initial_guess.append(attr)
             except:
                 continue
+    param_bounds = [tuple(param_lb), tuple(param_ub)]
     if typeof == 'least_squares':
-        res = least_squares(get_rms, initial_guess, args=(geoparams, raygroup, vary_dicts), max_nfev=max_iter)
-    new_params = update_geometry(res.x, geoparams, vary_dicts)
+        res = least_squares(get_rms, initial_guess, args=(geoparams, vary_dicts), max_nfev=max_iter,\
+                            bounds = param_bounds) 
+    new_params = update_geometry(np.around(res.x, 3), geoparams, vary_dicts)
     return new_params
