45982802abaa0a792d3b2c2a5cffeff4a383effe
GNiendorf <gavinniendorf@gmail.com>
2019-08-01 13:25:54 -0600

    code cleanup

--- tests/test_hyperbolic.py
+++ tests/test_hyperbolic.py
@@ -1,8 +1,3 @@
-import pytest
-
-import numpy as np
-import matplotlib.pyplot as plt
-
 import tracepy as tp
 
 lens = {
--- tracepy/geometry.py
+++ tracepy/geometry.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy import cos, sin, pi, sqrt
+from numpy import cos, sin, sqrt
 
 from .transforms import *
 
@@ -10,7 +10,7 @@ class geometry:
         self.D = np.array(params.get('D', [0., 0., 0.])) #Rotation angles
         self.action = params['action'] #Interaction with surface
         self.Diam = params['Diam'] #Outer diameter
-        self.N = params.get('N', 1.) #Index of refraction     
+        self.N = params.get('N', 1.) #Index of refraction
         self.kappa = params.get('kappa', None) #Specifies type of conic
         self.diam = params.get('diam', 0.) #Inner diameter
         self.c = params.get('c', 0.) #Vertex curvature. Default to plane.
@@ -28,7 +28,8 @@ class geometry:
 
     def check_params(self):
         """ Check that required parameters are given and update needed parameters. """
-        if isinstance(self.P, float) or isinstance(self.P, int): #Allow on axis integer for P.
+        if isinstance(self.P, float) or isinstance(self.P, int):
+            #Allow on axis integer for P.
             self.P = np.array([0., 0., self.P])
         else:
             self.P = np.array(self.P)
@@ -36,10 +37,11 @@ class geometry:
             if self.kappa is None:
                 raise Exception("Specify a kappa for this conic.")
             elif self.kappa>0:
-                print("Warning: Specified c value is not used when kappa>0")  
-                self.c = sqrt(1/(self.kappa*pow(self.Diam/2.,2))) 
+                print("Warning: Specified c value is not used when kappa>0")
+                self.c = sqrt(1/(self.kappa*pow(self.Diam/2.,2)))
         elif self.c == 0 and self.kappa is None:
-            self.kappa = 1. #Used for planes, does not affect calculations.       
+            #Used for planes, does not affect calculations.
+            self.kappa = 1.
 
     def get_surface(self, point):
         """ Returns the function and derivitive of a surface for a point. """
@@ -50,18 +52,20 @@ class geometry:
         return self.conics_plot(points)
     
     def conics(self, point):
-        """ Returns function and derivitive for conics and sphere surfaces. """
+        """ Returns function value and derivitive list for conics and sphere surfaces. """
         X,Y,Z = point
         rho = sqrt(pow(X,2) + pow(Y, 2))
         if rho > self.Diam/2. or rho < self.diam/2.: #Not on surface
             raise Exception()
-        function = Z - self.c*pow(rho, 2)/(1 + pow((1-self.kappa*pow(self.c, 2)*pow(rho,2)), 0.5)) #Conic equation.
-        E = self.c / pow((1-self.kappa*pow(self.c, 2)*pow(rho,2)), 0.5) #See Spencer, Murty section on rotational surfaces.
+        #Conic equation.
+        function = Z - self.c*pow(rho, 2)/(1 + pow((1-self.kappa*pow(self.c, 2)*pow(rho,2)), 0.5))
+        #See Spencer, Murty section on rotational surfaces for definition of E.
+        E = self.c / pow((1-self.kappa*pow(self.c, 2)*pow(rho,2)), 0.5)
         derivitive = [-X*E, -Y*E, 1.]
         return function, derivitive
 
     def conics_plot(self, point):
-        """ Returns Z value for an array of points for plotting conics. """
+        """ Returns Z values for an array of points for plotting conics. """
         X, Y = point[:,0], point[:,1]
         rho = sqrt(pow(X,2) + pow(Y,2))
         function = np.zeros(len(point))
@@ -69,4 +73,4 @@ class geometry:
         rho = sqrt(pow(X[~nan_idx],2) + pow(Y[~nan_idx], 2))
         function[nan_idx] = np.nan
         function[~nan_idx] = self.c*pow(rho, 2)/(1 + pow((1-self.kappa*pow(self.c, 2)*pow(rho,2)), 0.5))
-        return function 
+        return function
