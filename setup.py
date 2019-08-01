4f79d62054075b78510af6d7b46116ec9676484f
GNiendorf <gavinniendorf@gmail.com>
2019-07-31 19:04:11 -0600

    use readme as long description

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
 setup(name='tracepy',
       version='0.1',
       description='Optical design software for python.',
       url='http://github.com/GNiendorf/tracepy',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Gavin Niendorf',
       author_email='gavinniendorf@gmail.com',
       license='MIT',
